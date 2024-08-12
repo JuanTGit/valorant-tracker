5cf4b32c4355e4ced6ab6f98b161e52f032aac9f
Juan <j.tejeda46@yahoo.com>
2024-08-12 18:43:12 -0500

    updated fields in tracking list

--- viewTrackingList.js
+++ viewTrackingList.js
@@ -11,7 +11,7 @@ export async function viewTrackingList(server_id, interaction){
 		const embed = new EmbedBuilder()
 			.setColor(0xff0000)
 			.addFields(
-				{ name: 'Tracked Users', value: users.join(', '), inline: true }
+				{ name: 'Tracked Users', value: users.join('\n'), inline: true }
 			)
 
 		for (let i=0; i < query.rows.length; i++){
@@ -25,3 +25,6 @@ export async function viewTrackingList(server_id, interaction){
         console.error('Error fetching data:', error);
 	}
 }
+
+
+viewTrackingList(server_id, interaction)
