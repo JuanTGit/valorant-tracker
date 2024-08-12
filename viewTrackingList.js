ba22de8a807716b56f155e4eb19b333a27619d94
Juan <j.tejeda46@yahoo.com>
2024-08-12 18:51:34 -0500

    updated fields in tracking list

--- viewTrackingList.js
+++ viewTrackingList.js
@@ -7,21 +7,24 @@ export async function viewTrackingList(server_id, interaction){
 
 	try {
 		const query = await pool.query('SELECT server_id, agent FROM trackers WHERE server_id = $1', [server_id])
-		const users = []
+		const users = query.rows.map(row => row.agent).filter(agent => agent.trim() !== '');
+
+		if (users.length === 0) {
+			await interaction.editReply('No tracked users found.')
+			return;
+		}
+		
 		const embed = new EmbedBuilder()
 			.setColor(0xff0000)
 			.addFields(
 				{ name: 'Tracked Users', value: users.join('\n'), inline: true }
 			)
 
-		for (let i=0; i < query.rows.length; i++){
-			users.push(query.rows[i].agent)
-		}
-
 		console.log(users)
 		await interaction.editReply({ embeds: [embed]})
 
 	} catch (error) {
-        console.error('Error fetching data:', error);
+		console.error('Error fetching data:', error);
+		await interaction.editReply(`Error fetching data: ${error.message}`);
 	}
 }
